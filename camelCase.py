9c00615fa78b1537c242da3d17c7ecd6a58c10b9
Edwin <edwin.r.warlock@gmail.com>
2021-09-14 19:18:02 -0500

    remove intermediary list between original sentence list and camel case string

--- camelCase.py
+++ camelCase.py
@@ -13,16 +13,13 @@ def camel_case(sentence):
     #put each word of sentence into a starter list
     sentence_list = sentence.split()
 
-    #make blank list for camel case sentence
-    sentence_camelCase = []
+    #make empty string for camel case sentence
+    sentence_camelCase = ""
 
-    #put the first word into the camelcase list in all lowercase
+    #put the first word into the camelcase string in all lowercase & remove from starter list
     first_word = sentence_list[0]
     first_word = first_word.lower()
-    sentence_camelCase.append(first_word)
-
-    #take the first word out of the starter list
-    sentence_list.pop(0)
+    sentence_camelCase += (first_word)
 
     #format the rest of the words in the starter list 
     for word in sentence_list:
@@ -30,16 +27,9 @@ def camel_case(sentence):
         word_end = word.lower() #put all words in lowercase
         word_end = word_end[1:len(word) + 1] #removes the first letter of each lowercase word
         word_camel = word_start + word_end #adds the first uppercase letter to the start of each lowercase word
-        sentence_camelCase.append(word_camel) #add each word to end of camelcase list
+        sentence_camelCase += (word_camel) #add each word to end of camelcase string
 
-    #blank string for final product to be printed from
-    final_sentence = ''
-
-    #add contents of camelcase list to string...
-    for word in sentence_camelCase:
-        final_sentence = final_sentence + word
-
-    return final_sentence
+    return sentence_camelCase
 
 
 def main():
